ebf256b7e51e0475ec54201ad51f7887fba6ef18
Zeshan Khattak <zeshan77@gmail.com>
2018-02-10 14:56:10 +0400

    Update ActivityMonitorServiceProvider.php

--- src/ActivityMonitorServiceProvider.php
+++ src/ActivityMonitorServiceProvider.php
@@ -13,7 +13,10 @@ class ActivityMonitorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->publishes([
+            __DIR__.'/database/migrations/' => database_path('migrations')
+        ], 'migrations');
+        
     }
 
     /**
