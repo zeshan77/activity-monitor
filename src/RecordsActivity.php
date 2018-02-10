d93fbe4bfc7a29fdd77e756e5e06f6ac6a237c7d
Zeshan Khattak <zeshan77@gmail.com>
2018-02-10 15:28:02 +0400

    Update RecordsActivity.php

--- src/RecordsActivity.php
+++ src/RecordsActivity.php
@@ -28,9 +28,30 @@ trait RecordsActivity
 
     protected function recordActivity($event)
     {
+
+        $after = $this->getDirty();
+        $before = [];
+        foreach ($this->getDirty() as $field => $value) {
+
+            $before[$field] = $this->getOriginal($field);
+
+        }
+
+        if($this->dontLogFields) {
+            foreach ($this->dontLogFields as $field) {
+                if (array_key_exists($field, $before)) unset($before[$field]);
+                if (array_key_exists($field, $after)) unset($after[$field]);
+            }
+        }
+
+        $after = json_encode($after);
+        $before = json_encode($before);
+
         $this->activity()->create([
             'user_id' => auth()->id(),
-            'type' => $this->getActivityType($event)
+            'type' => $this->getActivityType($event),
+            'before' => $before,
+            'after' => $after
         ]);
     }
 
@@ -43,7 +64,7 @@ trait RecordsActivity
      * @param $event
      * @return string
      */
-    protected function getActivityType($event): string
+    protected function getActivityType($event)
     {
         $class = strtolower((new \ReflectionClass($this))->getShortName());
         return "{$event}_{$class}";
